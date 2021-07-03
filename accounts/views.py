56466fa03a116a822edf842198534235a4e30930
yohei <yohei_green@cloud.com>
2021-07-04 00:00:06 +0900

    signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import LoginForm
+from django.http import HttpResponse, request
+
 
 class Login(LoginView):
     """ Login Page """
@@ -9,7 +12,18 @@ class Login(LoginView):
     form_class = LoginForm
     template_name = 'login.html'
 
+
+def signup(request):
+    return render(request, 'registration/signup.html')
+        
+
+
+
+
+
 class Logout(LoginRequiredMixin, LogoutView):
     """ Logout Page """
 
     template_name = 'login.html'
+
+
